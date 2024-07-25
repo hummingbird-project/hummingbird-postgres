c99743b50ac28bf8ac3f7f8e3a44deabcd971752
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2024-07-25 07:18:52 -0400

    chore(format): swift format

--- Sources/JobsPostgres/Migrations/CreateJobs.swift
+++ Sources/JobsPostgres/Migrations/CreateJobs.swift
@@ -29,7 +29,6 @@ struct CreateJobs: PostgresMigration {
             """,
             logger: logger
         )
-
         try await connection.query(
             """
             CREATE INDEX IF NOT EXISTS _hb_job_status
