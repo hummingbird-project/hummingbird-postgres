27cb90b31e2551760514d9eac000849c7f7d09af
Adam Fowler <adamfowler71@gmail.com>
2025-10-23 12:09:28 +0100

    Use postgres-migrations 1.1.0

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.16.0"),
-        .package(url: "https://github.com/hummingbird-project/postgres-migrations", from: "1.0.0"),
+        .package(url: "https://github.com/hummingbird-project/postgres-migrations", from: "1.1.0"),
         .package(url: "https://github.com/vapor/postgres-nio", from: "1.29.0"),
     ],
     targets: [
